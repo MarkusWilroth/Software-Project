8d31a0f70eba4ebc570abcd832f9562ed360e1f8
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-05-14 12:01:47 +0200

    GridManager

--- Unity/Project/Assets/GridManager.cs
+++ Unity/Project/Assets/GridManager.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour {
 
+	private int rows = 5;
+	private int cols = 8;
+	private float tileSize = 1;
+
 	// Use this for initialization
 	void Start () {
-		
+		GenerateGrid();
 	}
-	
+
+	private void generateGrid() {
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+				
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update () {
 		
