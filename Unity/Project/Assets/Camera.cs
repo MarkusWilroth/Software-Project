a81d167a3e243a0f2bf6f128ffa858252ee3ff41
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-05-15 11:27:04 +0200

    Update Camera

--- Unity/Project/Assets/Camera.cs
+++ Unity/Project/Assets/Camera.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public float panSpeed = 7f;
+	public float panBorderThickness = 5f;
+
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 pos = transform.position;
+		if (Input.GetKey ("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) {
+			pos.y += panSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey ("s") || Input.mousePosition.y <= panBorderThickness) {
+			pos.y -= panSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey ("d") || Input.mousePosition.x >= Screen.height - panBorderThickness) {
+			pos.x += panSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey ("a") || Input.mousePosition.x <= panBorderThickness) {
+			pos.x -= panSpeed * Time.deltaTime;
+		}
+
+		transform.position = pos;
 	}
 }
