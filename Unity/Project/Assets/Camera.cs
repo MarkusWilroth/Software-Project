812de3e9c56c945812788c4713db3cdec8ff000d
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-05-15 11:58:37 +0200

    Zoom

--- Unity/Project/Assets/Camera.cs
+++ Unity/Project/Assets/Camera.cs
@@ -8,6 +8,9 @@ public class Camera : MonoBehaviour {
 	public float panBorderThickness = 5f;
 	public Vector2 panLimit;
 
+	public float scrollSpeed = 2f;
+	public float minY = 20f;
+	public float maxY = 120f;
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,7 +28,11 @@ public class Camera : MonoBehaviour {
 			pos.x -= panSpeed * Time.deltaTime;
 		}
 
+		float scroll = Input.GetAxis ("Mouse ScrollWheel");
+		pos.z += scroll * scrollSpeed * 50f * Time.deltaTime;
+
 		pos.x = Mathf.Clamp (pos.x, -panLimit.x, panLimit.x);
+		pos.z = Mathf.Clamp (pos.z, minY, maxY);
 		pos.y = Mathf.Clamp (pos.y, -panLimit.y, panLimit.y);
 
 		transform.position = pos;
