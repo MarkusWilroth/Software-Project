0457046c730f4c10bcb70126476168725b0df31d
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-05-17 01:04:25 +0200

    Påbörjad Castle och fiende script
    Måste kollen en massa tutorials....

---
+++ Unity/Project/Assets/Scripts/Castle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castle : MonoBehaviour {
+    public int maxHP = 200;
+    public int HP = 0;
+    public int armor = 10; //procent av skada som räknas bort (halverad effect för AP damage)
+    public int damage = 0;
+    public int range = 0;
+
+    public bool towerUpgrade; //dessa kommer förmodligen att ändras beroende på vad tutorials visar
+    public bool fireArrows;
+
+    void Start() {
+        HP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (HP <= 0) {
+            GameOver();
+        }
+    }
+
+    void GameOver () {
+        //Avslutar spelet/leveln
+    }
+
+    public bool Repair() {
+        //Ska gå att kommas åt från HUD, betalar man för repair ska
+        if (maxHP < HP) { //Se till att om skada tas går den över till repair is done för att inte ha ett odödligt fort
+            //Repairing
+            return false;
+        }
+        else {
+            //repair is done
+            return true;
+        }
+        
+    }
+
+    public void takeDamage (int damage, bool AP) {
+        if (AP) {
+            damage -= (damage * (armor / 2));
+        }
+        else {
+            damage -= damage * armor;
+        }
+
+        HP -= damage;
+         
+    }
+}
---
+++ Unity/Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int maxHP = 20;
+    public int HP = 20;
+    public int Armor = 0;
+    public int damage = 10;
+    public int maxFollowers = 0;
+    public int followers = 0;
+    public int followerLvl = 0;
+    public int range = 5;
+    public float speed = 10f;
+    public Vector2 pos = new Vector2(0,0);
+
+    void Start() {
+        HP = maxHP;
+        //pos = map.SpawnPoint;
+    }
+    
+    void Update() {
+        if (followers < maxFollowers) {
+            Recruit();
+        }
+        //Funderar på om det inte är bäst att ha alla fiender i ett o samma script... vi får kolla på tutorials vad som är bäst
+    }
+
+    void Recruit() {
+        //Skapar en ny fiende o ger dess stats beroende på vilka stats den kan ha
+        followers++;
+    }
+}
