6fe78fca767e341003bc1329064213dbe8dd0f1d
Fabian Ramel <37147537+Fobian1@users.noreply.github.com>
2019-05-20 13:14:23 +0200

    Scripts update

---
+++ Unity/Project/Assets/Scripts/Units/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+    //public enum states {
+    //    attackEnemy,
+    //    retreat
+    //}  Inte säker på att vi ska använda states?
+
+    public int maxHP, HP, armour, damage, soldiers, maxSoldiers, soldiersLvl, Exp, range;
+    private float speed;
+    private bool unlocks, traits, weapon, gatherTroops, inRange;
+    private Castle castle;
+    public Transform heroPos;
+
+    void Start() {
+        HP = maxHP;
+        castle = GameObject.FindGameObjectWithTag("Castle").GetComponent<Castle>();
+        // pos = map.Spawnpoint
+    }
+
+    void Update() {
+        heroPos = GameObject.FindGameObjectWithTag("Enemy").transform;
+        if (soldiers < maxSoldiers) {
+            Recruit();
+        }
+        if (HP <= 0) {
+            Respawn();
+        }
+        if (gatherTroops) { //Kan vara ett bra sätt att samla sina heros och soldiers
+            heroPos = GameObject.FindGameObjectWithTag("Castle").transform;
+        }
+
+    }
+
+    void Recruit() {
+        soldiers++;
+    }
+    void Respawn() {
+        // Timer som startar innan den får komma tillbaka        
+        Start();
+    }
+}
---
+++ Unity/Project/Assets/Scripts/Units/Soldiers.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldiers : MonoBehaviour
+{
+    private int weapon, soldierLvl, HP, maxHP;
+    private float speed;
+    bool retreat;
+    private Hero hero;
+    private Transform soldierPos;
+
+    void Start()
+    {
+        HP = maxHP;
+        hero = GameObject.FindGameObjectWithTag("Hero").GetComponent<Hero>();
+        ////soldierPos = GameObject.FindGameObjectWithTag("Hero").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        soldierPos = GameObject.FindGameObjectWithTag("Hero").transform;
+    }
+}
