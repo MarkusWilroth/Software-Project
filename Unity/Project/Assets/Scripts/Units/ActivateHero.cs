bf73639fd0d6e475cc507e7b6f17d80c4222a8f7
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-05-28 09:45:57 +0200

    Kommentarer

--- Unity/Project/Assets/Scripts/Units/ActivateHero.cs
+++ Unity/Project/Assets/Scripts/Units/ActivateHero.cs
@@ -6,14 +6,14 @@ public class ActivateHero : MonoBehaviour {
 
     public GameObject rangeHero, warriorHero;
 
-    public void Activate(int id) {
-        rangeHero = GameObject.FindGameObjectWithTag("RangeHero");
+    public void Activate(int id) { //Får in värde 1 eller 2 beroende på vilken knapp man klickar på
+        rangeHero = GameObject.FindGameObjectWithTag("RangeHero"); //Borde inte vara annorlund än hur vi gör detta i EnemyLeader, hade tänkt att detta skulle vara en sorts FindChild men hittade ingen sådan
         warriorHero = GameObject.FindGameObjectWithTag("WarriorHero");
 
 
         switch (id) {
-            case 1:
-                rangeHero.MakeActive();
+            case 1: //Om id = 1 så klickade man på ranger knappen och ranger ska bli activerad
+                rangeHero.MakeActive(); //Ska finnas en metod i Hero som ska fixa detta så som vi gör TakeDamage
                 warriorHero.DeActivate();
                 break;
             case 2:
