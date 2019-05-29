7a602737404ecd1b692c175b437824186358199b
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2019-05-29 10:54:33 +0200

    Start Menu fixad

---
+++ Unity/Project/Assets/Scripts/Buttons/MenyButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenyButtons : MonoBehaviour {
+    public GameObject startMenu, optionMenu, helpMenu;
+
+    void Start() {
+        startMenu = GameObject.Find("StartMenu");
+        optionMenu = GameObject.Find("OptionMenu");
+        helpMenu = GameObject.Find("HelpMenu");
+
+        optionMenu.SetActive(false);
+        helpMenu.SetActive(false);
+    }
+
+    public void changeMenuScene(string sceneName) {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+        //Scene nextScene = SceneManager.GetSceneByName(sceneName);
+        //Debug.Log("String sceneName: " + sceneName + " Scene nextScene: " + nextScene);
+        //SceneManager.SetActiveScene(nextScene);
+    }
+    public void Help() {
+        helpMenu.SetActive(true);
+        optionMenu.SetActive(false);
+    }
+    public void Options() {
+        optionMenu.SetActive(true);
+        startMenu.SetActive(false);
+    }
+    public void Return() {
+        startMenu.SetActive(true);
+        optionMenu.SetActive(false);
+        helpMenu.SetActive(false);
+    }
+    public void Quit() {
+        Application.Quit();
+    }
+
+}
